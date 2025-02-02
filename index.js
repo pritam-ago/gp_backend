f0a3ad199880aad3c23cd240414c623b706801ed
Vikas Pritam <pritam.developer.1@gmail.com>
2025-02-02 19:46:13 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoutes from "./routes/authRoutes";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
