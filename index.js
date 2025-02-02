955c0edb5ff655b7f2d74da72e2718c744e78d57
Vikas Pritam <pritam.developer.1@gmail.com>
2025-02-02 18:56:06 +0530

    .

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 import express, { json } from "express";
 import { connect } from "mongoose";
 import cors from "cors";
-require("dotenv").config();
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
-app.use(json());
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:5000',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials : true
+}
+app.use(express.json());
+app.use(cors(corsOptions));
 
 connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.use("/auth", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
